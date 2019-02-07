ca2049967b99edb5fba6b458f8eb4c9b1a02fdbe
= <=>
2019-02-07 11:49:23 -0700

    Added accessibility features, help tab, and puppies photo

--- phone.js
+++ phone.js
@@ -2,22 +2,33 @@ $(document).ready(function() { // do this when the document is loaded
 	$("#dialer").show();
 	$("#contacts").hide();
   $("#add_contact").hide();
+	$("#help").hide();
 });
 
 $("#tab_1_button").click(function() { // when "tab_1" is clicked
 	$("#dialer").show();
   $("#contacts").hide();
   $("#add_contact").hide();
+	$("#help").hide();
 });
 
 $("#tab_2_button").click(function() { // when "tab_2" is clicked
   $("#contacts").show();
   $("#dialer").hide();
   $("#add_contact").hide();
+	$("#help").hide();
 });
 
 $("#tab_3_button").click(function() { // when "tab_3" is clicked
   $("#add_contact").show();
+	$("#dialer").hide();
+  $("#contacts").hide();
+	$("#help").hide();
+});
+
+$("#help_button").click(function() { // when "tab_3" is clicked
+	$("#help").show();
+  $("#add_contact").hide();
 	$("#dialer").hide();
   $("#contacts").hide();
 });
